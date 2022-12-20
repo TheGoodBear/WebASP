45c46e29c1d00045ee68f114a38e5389c3f2699e
TheGoodBear <alain@micalaudie.com>
2022-12-20 13:50:56 +0100

    Finalisation MVVM avec hétitage

---
+++ MVMVC-EF/ViewModels/GenericVM.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVMVC_EF.Data;
+using MVMVC_EF.Models;
+using System.ComponentModel.DataAnnotations;
+using MVMVC_EF.Utilities;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace MVMVC_EF.ViewModels
+{
+    public class GenericVM
+    {
+        public enum eView
+        {
+            [Display(Name = "Liste")]
+            Index,
+            [Display(Name = "Détail")]
+            Detail,
+            [Display(Name = "Création")]
+            Create,
+            [Display(Name = "Modification")]
+            Edit,
+            [Display(Name = "Suppression")]
+            Delete
+        }
+
+
+        // views properties
+        public string EntityName { get; set; } = "";
+        public string EntityNamePlural { get; set; }
+
+        public string IndexTitle { get; set; }
+        public string DetailTitle { get; set; }
+        public string CreateTitle { get; set; } 
+        public string EditTitle { get; set; } 
+        public string DeleteTitle { get; set; } 
+        public string? PageTitle { get; set; }
+        public string TabTitle { get; set; }
+        public string CreateText { get; set; }
+        public string EditText { get; set; } = $"Modifier";
+        public string EditIcon { get; set; } = $"bi-pencil-square";
+        public string DeleteText { get; set; } = $"Supprimer";
+        public string DeleteIcon { get; set; } = $"bi-trash";
+        public string DeleteConfirmText { get; set; }
+        public string GoToListText { get; set; } = $"Retour à la liste";
+        public string AddText { get; set; } = $"Ajouter";
+
+        public List<Tuple<string, string>> ViewData { get; set; } = new List<Tuple<string, string>>();
+
+
+        // models properties
+        public DBContext? Context { get; set; }
+
+
+        // constructor
+        public GenericVM()
+        { }
+        public GenericVM(
+            DBContext Context,
+            eView View)
+        {
+
+            this.Context = Context;
+
+            ViewData.Add(new Tuple<string, string>(
+                "TabTitle", TabTitle));
+            ViewData.Add(new Tuple<string, string>(
+                "ProjectVisible", ""));
+
+        }
+
+
+        public void UpdateControllerData(string EntityName, eView View)
+        {
+
+            EntityNamePlural = $"{EntityName}s";
+            IndexTitle = $"{eView.Index.GetDisplayName()} des [Nb] {EntityNamePlural}";
+            DetailTitle = $"{eView.Detail.GetDisplayName()} du {EntityName} [Entity]";
+            CreateTitle = $"{eView.Create.GetDisplayName()} d'un nouveau {EntityName}";
+            EditTitle = $"{eView.Edit.GetDisplayName()} du {EntityName} [Entity]";
+            DeleteTitle = $"{eView.Delete.GetDisplayName()} du {EntityName} [Entity]";
+
+            CreateText = $"Ajouter un nouveau {EntityName}";
+            DeleteConfirmText = $"Êtes-vous sûr de vouloir supprimer ce {EntityName} ?";
+
+            switch (View)
+            {
+                case eView.Index:
+                    PageTitle = IndexTitle;
+                    TabTitle = $"{eView.Index.GetDisplayName()} {EntityName}";
+                    break;
+                case eView.Detail:
+                    PageTitle = DetailTitle;
+                    TabTitle = $"{eView.Detail.GetDisplayName()} {EntityName}";
+                    break;
+                case eView.Create:
+                    PageTitle = CreateTitle;
+                    TabTitle = $"{eView.Create.GetDisplayName()} {EntityName}";
+                    break;
+                case eView.Edit:
+                    PageTitle = EditTitle;
+                    TabTitle = $"{eView.Edit.GetDisplayName()} {EntityName}";
+                    break;
+                case eView.Delete:
+                    PageTitle = DeleteTitle;
+                    TabTitle = $"{eView.Delete.GetDisplayName()} {EntityName}";
+                    break;
+            }
+
+        }
+
+    }
+}
--- MVMVC-EF/ViewModels/GroupVM.cs
+++ MVMVC-EF/ViewModels/GroupVM.cs
@@ -6,48 +6,14 @@ using MVMVC_EF.Utilities;
 
 namespace MVMVC_EF.ViewModels
 {
-    public class GroupVM
+    public class GroupVM : GenericVM
     {
-        public enum eView
-        {
-            [Display(Name = "Liste")]
-            Index,
-            [Display(Name = "Détail")]
-            Detail,
-            [Display(Name = "Création")]
-            Create,
-            [Display(Name = "Modification")]
-            Edit,
-            [Display(Name = "Suppression")]
-            Delete
-        }
-
 
         // views properties
-        public static string EntityName = "groupe";
-        public static string EntityNamePlural = $"{EntityName}s";
-
-        public static string IndexTitle = $"{eView.Index.GetDisplayName()} des [Nb] {EntityNamePlural}";
-        public static string DetailTitle = $"{eView.Detail.GetDisplayName()} du {EntityName} [Entity]";
-        public static string CreateTitle = $"{eView.Create.GetDisplayName()} d'un nouveau {EntityName}";
-        public static string EditTitle = $"{eView.Edit.GetDisplayName()} du {EntityName} [Entity]";
-        public static string DeleteTitle = $"{eView.Delete.GetDisplayName()} du {EntityName} [Entity]";
-        public string? PageTitle { get; set; }
-        public static string TabTitle;
-        public string CreateText { get; set; } = $"Ajouter un nouveau {EntityName}";
-        public string EditText { get; set; } = $"Modifier";
-        public string EditIcon { get; set; } = $"bi-pencil-square";
-        public string DeleteText { get; set; } = $"Supprimer";
-        public string DeleteIcon { get; set; } = $"bi-trash";
-        public string DeleteConfirmText { get; set; } = $"Êtes-vous sûr de vouloir supprimer ce {EntityName} ?";
-        public string GoToListText { get; set; } = $"Retour à la liste";
-        public string AddText { get; set; } = $"Ajouter";
-
-        public List<Tuple<string, string>> ViewData { get; set; } = new List<Tuple<string, string>>();
+        public string EntityName { get; set; } = "groupe";
  
 
         // models properties
-        public DBContext? Context { get; set; }
         public List<Group>? Entities { get; set; }
         private int? _IDEntity;
         public int? IDEntity 
@@ -70,44 +36,17 @@ namespace MVMVC_EF.ViewModels
 
 
         // constructor
-        public GroupVM()
+        public GroupVM() : base()
         { }
         public GroupVM(
             DBContext Context,
-            eView View)
+            eView View) 
+            : base(Context, View)
         {
 
-            switch (View)
-            {
-                case eView.Index:
-                    PageTitle = IndexTitle;
-                    TabTitle = $"{eView.Index.GetDisplayName()} {EntityName}";
-                    break;
-                case eView.Detail:
-                    PageTitle = DetailTitle;
-                    TabTitle = $"{eView.Detail.GetDisplayName()} {EntityName}";
-                    break;
-                case eView.Create:
-                    PageTitle = CreateTitle;
-                    TabTitle = $"{eView.Create.GetDisplayName()} {EntityName}";
-                    break;
-                case eView.Edit:
-                    PageTitle = EditTitle;
-                    TabTitle = $"{eView.Edit.GetDisplayName()} {EntityName}";
-                    break;
-                case eView.Delete:
-                    PageTitle = DeleteTitle;
-                    TabTitle = $"{eView.Delete.GetDisplayName()} {EntityName}";
-                    break;
-            }
-
-            this.Context = Context;
             Entities = Context.Group.ToList();
 
-            ViewData.Add(new Tuple<string, string> (
-                "TabTitle", TabTitle));
-            ViewData.Add(new Tuple<string, string>(
-                "ProjectVisible", ""));
+            UpdateControllerData(EntityName, View);
 
             PageTitle = PageTitle
                 .Replace("[Nb]", Entities.Count().ToString());
